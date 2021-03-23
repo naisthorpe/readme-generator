ee8eeb8be1630f9c9f98618470a2fcb09743d27f
naisthorpe <aisthorpe@gmail.com>
2021-03-23 11:32:32 -0400

    Add generateMarkdown to required packages on index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -59,7 +60,7 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    
+
 }
 
 // Function call to initialize app

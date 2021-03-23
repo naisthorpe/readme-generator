61b9be39495c7f330cf22bbf2a884c291a2ead5e
naisthorpe <aisthorpe@gmail.com>
2021-03-23 11:24:55 -0400

    Add package requirements and build inquirer array of questions

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "list",
+        message: "What license did you use?",
+        name: "license",
+        choices: [
+            "Apache",
+            "Boost",
+            "BSD 3",
+            "BSD 2",
+            "Eclipse",
+            "IBM",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Perl",
+            "Artistic",
+            "SIL",
+            "Unlicense",
+            "WTFPL",
+            "Zlib",
+            "None"
+        ],
+    },
+    {
+        type: "input",
+        message: "Add your project description:",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Add usage text:",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "contributors",
+    },
+    {
+        type: "input",
+        message: "Enter your email:",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "github",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
